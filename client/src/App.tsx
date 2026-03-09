f031f03e2072b10eda6b7fda9c1a6df3a92bd83f
Sakthivaishnavi <sakthivaishnavi12@gmail.com>
2026-03-09 14:40:09 +0530

    feat: instructor dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,9 @@ import HomePage from "./pages/HomePage";
 import AllCoursesPage from "./pages/AllCoursesPage";
 import CourseDetailPage from "./pages/CourseDetailPage";
 import ProfilePage from "./pages/ProfilePage";
+import InstructorPage from "./pages/InstructorPage";
+import CreateCoursePage from "./pages/CreateCoursePage";
+import CreateQuizPage from "./pages/CreateQuizPage";
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
@@ -26,6 +29,9 @@ function App() {
           <Route element={<ProtectedRoutes />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/user/profile/:id" element={<ProfilePage />} />
+            <Route path="/instructor" element={<InstructorPage />} />
+            <Route path="/instructor/createCourse" element={<CreateCoursePage />} />
+            <Route path="/instructor/createQuiz" element={<CreateQuizPage />} />
           </Route>
 
           <Route path="/courses/:section" element={<AllCoursesPage />} />
