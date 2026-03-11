c4b5eb20e63f0906eb3371059dd698a3ca185c75
santhosh mani <saisanthoshmani@jmangroup.com>
2026-03-11 12:26:12 +0530

    fix(terraform): Update provider version and remove obsolete state file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+
+  backend "azurerm" {
+     storage_account_name = "group14tfstorage"
+     resource_group_name  = "group14_mini_project"
+     container_name       = "group14-tfstate-storage"
+     key                  = "terraform.tfstate"
+  } 
+}
+
+
 provider "azurerm" {
   features {}
 }
